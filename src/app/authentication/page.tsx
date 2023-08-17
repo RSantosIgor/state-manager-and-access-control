a777d09b8e9d09c55349a0b411824bd14f02c920
RSantosIgor <s.igorrosa@gmail.com>
2023-08-17 10:16:59 -0300

    Authentications pages

---
+++ src/app/authentication/page.tsx
@@ -0,0 +1,52 @@
+"use client";
+import { Metadata } from "next";
+import Link from "next/link";
+import { useState } from "react";
+import SignIn from "../components/authentication/signIn";
+import SignUp from "../components/authentication/signUp";
+
+export const metadata: Metadata = {
+  title: "Authentication"
+}
+
+export default function AuthenticationPage() {
+  const [isSignIn, setIsSignIn] = useState(true);
+  
+  const toogleSign = () => {
+    setIsSignIn(!isSignIn);
+  }
+
+  return (
+    <>
+      <div className="container relative hidden min-h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+        <Link
+          onClick={toogleSign}
+          href="#"
+          className={
+            "absolute right-4 top-4 md:right-8 md:top-8"
+          }
+        >
+          {isSignIn ? 'Cadastrar': 'Entrar'}
+        </Link>
+        <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
+          <div className="absolute inset-0 bg-zinc-900" />
+          <div className="relative z-20 flex items-center text-lg font-medium">
+            TCC Project
+          </div>
+        </div>
+        <div className="lg:p-8 sm:mx-auto sm:w-full sm:max-w-sm">
+          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+            {isSignIn ? <SignIn/>: <SignUp/>}
+            <p className="mt-10 text-center text-sm text-gray-200">
+              {isSignIn ? 'Não tenho uma conta ': 'Já tenho uma conta '}
+              <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+                onClick={toogleSign}>
+                {isSignIn ? 'fazer uma nova conta': 'entrar na minha conta'}
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
---
+++ src/app/components/authentication/signUp/index.tsx
@@ -0,0 +1,75 @@
+
+export default function SignUp() {
+    return (
+      <div>
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">
+            Cadastrar conta
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" action="#" method="POST">
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-white">
+                Nome
+              </label>
+              <div className="mt-2">
+                <input
+                  id="name"
+                  name="name"
+                  type="name"
+                  autoComplete="name"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-white">
+                Email
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-white">
+                  Senha
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Cadastrar
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+}
